ffaec7d094fb6469fcc0948394efde1dc9dcb2af
Naufal Alief <naufalalief087@gmail.com>
2024-05-11 04:11:03 +0700

    position error

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Page = ({ children }) => {
   return (
-    <div className="relative w-64 mx-auto my-52 p-52 h-72 flex items-center justify-center">
+    <div className="relative w-32 mx-auto my-52 p-52 h-72 flex items-center justify-center">
       {children}
     </div>
   );
