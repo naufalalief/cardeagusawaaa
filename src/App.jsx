3a96a7a6a7b58c849691ba3da4b08ba1f83c840f
Naufal Alief <naufalalief087@gmail.com>
2024-05-11 19:36:38 +0700

    atek kalkulasi

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import Card from "./components/card/Card.jsx";
 import Page from "./components/layout/Page.jsx";
 const App = () => {
+  const directions = ["left", "right", "top", "bottom"];
   return (
     <div className="">
       <Page>
-        <Card />
+        <Card
+          movementDirections={directions}
+          initialPosition={{ x: 0, y: 0 }}
+        />
       </Page>
     </div>
   );
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,21 +6,40 @@ const Card = () => {
   const [posX, setPosX] = useState(0);
   const [posY, setPosY] = useState(0);
 
+  // const handlePosition = (e) => {
+  //   const rect = scope.current.getBoundingClientRect();
+  //   const clientX = e.clientX;
+  //   const clientY = e.clientY;
+  //   const padding = 50;
+
+  //   if (clientX < rect.left + padding) {
+  //     setPosX(10);
+  //   } else if (clientX > rect.right - padding) {
+  //     setPosX(-10);
+  //   } else if (clientY < rect.top + padding) {
+  //     setPosY(10);
+  //   } else if (clientY > rect.bottom - padding) {
+  //     setPosY(-10);
+  //   }
+  // };
+
   const handlePosition = (e) => {
     const rect = scope.current.getBoundingClientRect();
-    const clientX = e.clientX;
-    const clientY = e.clientY;
-    const padding = 10;
+    const parentRect = scope.current.parentElement.getBoundingClientRect();
+    const movespeed = 10; // aturen speed e mek
+    const relativeX = e.clientX - parentRect.left - rect.width / 2;
+    const relativeY = e.clientY - parentRect.top - rect.width / 2;
+    const movementX =
+      relativeX > 0
+        ? Math.min(relativeX, -movespeed)
+        : Math.max(relativeX, movespeed);
 
-    if (clientX < rect.left + padding) {
-      setPosX(2);
-    } else if (clientX > rect.right - padding) {
-      setPosX(-2);
-    } else if (clientY < rect.top + padding) {
-      setPosY(2);
-    } else if (clientY > rect.bottom - padding) {
-      setPosY(-2);
-    }
+    const movementY =
+      relativeY > 0
+        ? Math.min(relativeY, -movespeed)
+        : Math.max(relativeY, movespeed);
+    setPosX(movementX);
+    setPosY(movementY);
   };
 
   const handleLeave = () => {
@@ -29,21 +48,27 @@ const Card = () => {
   };
 
   useEffect(() => {
-    animate("#target", {
-      x: posX,
-      y: posY,
-    });
+    animate(
+      "#target",
+      {
+        x: posX,
+        y: posY,
+      },
+      {
+        duration: 1,
+      }
+    );
   }, [posX, posY]);
 
   return (
     <div
-      className="absolute bg-rose-50 p-6 rounded"
+      className="absolute bg-rose-50 p-10 rounded"
       ref={scope}
       onMouseMove={handlePosition}
       onMouseLeave={handleLeave}
     >
       <div
-        className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        className="w-64 h-64 p-6 flex items-center justify-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
         id="target"
       >
         Test
